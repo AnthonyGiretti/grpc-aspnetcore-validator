020ed2df55bd29a49a76623a146b831d8dbaa571
[MVP] Anthony Giretti <anthony.giretti@gmail.com>
2020-01-22 22:48:55 -0500

    Create GrpcProtocolConstants.cs

---
+++ src/Grpc.AspNetCore.FluentValidation/Internal/GrpcProtocolConstants.cs
@@ -0,0 +1,8 @@
+namespace Grpc.AspNetCore.FluentValidation.Internal
+{
+    internal static class GrpcProtocolConstants
+    {
+        internal const string StatusTrailerName = "grpc-status";
+        internal const string StatusTrailerInvalidArgument = "3";
+    }
+}
