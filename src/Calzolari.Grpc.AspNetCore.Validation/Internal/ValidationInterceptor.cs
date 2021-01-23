b860d8054a912d43675ebab90ee45a5ad8df13d9
Anthony Giretti <anthony.giretti@oneocean.com>
2021-01-22 22:28:43 -0500

    implement validation on streamed messages

--- src/Calzolari.Grpc.AspNetCore.Validation/Internal/ValidationInterceptor.cs
+++ src/Calzolari.Grpc.AspNetCore.Validation/Internal/ValidationInterceptor.cs
@@ -16,23 +16,46 @@ namespace Calzolari.Grpc.AspNetCore.Validation.Internal
             _handler = handler;
         }
 
-        public override async Task<TResponse> UnaryServerHandler<TRequest, TResponse>(
-            TRequest request,
-            ServerCallContext context,
-            UnaryServerMethod<TRequest, TResponse> continuation)
+        public override async Task<TResponse> UnaryServerHandler<TRequest, TResponse>(TRequest request, 
+                                                                                      ServerCallContext context, 
+                                                                                      UnaryServerMethod<TRequest, TResponse> continuation)
         {
             await ValidateRequest(request);
             return await continuation(request, context);
         }
 
-        public override async Task ServerStreamingServerHandler<TRequest, TResponse>(
-            TRequest request, 
-            IServerStreamWriter<TResponse> responseStream,
-            ServerCallContext context, 
-            ServerStreamingServerMethod<TRequest, TResponse> continuation)
+        public override async Task ServerStreamingServerHandler<TRequest, TResponse>(TRequest request, 
+                                                                                     IServerStreamWriter<TResponse> responseStream, 
+                                                                                     ServerCallContext context, 
+                                                                                     ServerStreamingServerMethod<TRequest, TResponse> continuation)
         {
             await ValidateRequest(request);
             await continuation(request, responseStream, context);
+        }
+
+        public override async Task<TResponse> ClientStreamingServerHandler<TRequest, TResponse>(IAsyncStreamReader<TRequest> requestStream, 
+                                                                                     ServerCallContext context, 
+                                                                                     ClientStreamingServerMethod<TRequest, TResponse> continuation)
+        {
+            await foreach (var message in requestStream.ReadAllAsync())
+            {
+                await ValidateRequest(message);
+            }
+
+            return await continuation(requestStream, context);
+        }
+
+        public override async Task DuplexStreamingServerHandler<TRequest, TResponse>(IAsyncStreamReader<TRequest> requestStream, 
+                                                                                     IServerStreamWriter<TResponse> responseStream, 
+                                                                                     ServerCallContext context, 
+                                                                                     DuplexStreamingServerMethod<TRequest, TResponse> continuation)
+        {
+            await foreach (var message in requestStream.ReadAllAsync())
+            {
+                await ValidateRequest(message);
+            }
+
+            await continuation(requestStream, responseStream, context);
         }
 
         private async Task ValidateRequest<TRequest>(TRequest request) where TRequest : class
