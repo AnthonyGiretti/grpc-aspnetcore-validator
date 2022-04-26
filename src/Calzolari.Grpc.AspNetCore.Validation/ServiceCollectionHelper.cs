48c1241b1255f33da0f14815e6af8358e59f007c
Meysam Moghaddam <meysam.mogaddam@gmail.com>
2022-04-27 00:53:35 +0430

    Add AddValidatorsFromAssembly In ServiceCollectionHelper Class

--- src/Calzolari.Grpc.AspNetCore.Validation/ServiceCollectionHelper.cs
+++ src/Calzolari.Grpc.AspNetCore.Validation/ServiceCollectionHelper.cs
@@ -3,6 +3,7 @@ using FluentValidation;
 using Microsoft.Extensions.DependencyInjection;
 using System;
 using System.Linq;
+using System.Reflection;
 
 namespace Calzolari.Grpc.AspNetCore.Validation
 {
@@ -59,6 +60,48 @@ namespace Calzolari.Grpc.AspNetCore.Validation
             Action<AbstractValidator<TMessage>> validator)
         {
             services.AddSingleton<IValidator<TMessage>>(new Validation.InlineValidator<TMessage>(validator));
+            return services;
+        }
+
+        /// <summary>
+        /// Adds all validators in specified assembly
+        /// </summary>
+        /// <param name="services">The collection of services</param>
+        /// <param name="assembly">The assembly to scan</param>
+        /// <param name="lifetime">The lifetime of the validators. The default is transient</param>
+        /// <param name="filter">Optional filter that allows certain types to be skipped from registration.</param>
+        /// <returns></returns>
+        public static IServiceCollection AddValidatorsFromAssembly(this IServiceCollection services,
+            Assembly assembly,
+            ServiceLifetime lifetime = ServiceLifetime.Transient,
+            Func<AssemblyScanner.AssemblyScanResult, bool> filter = null)
+        {
+            foreach (var scanResult in AssemblyScanner.FindValidatorsInAssembly(assembly).Where(filter ?? (_ => true)))
+                services.AddValidator(scanResult.ValidatorType);
+            return services;
+        }
+
+        /// <summary>
+        ///     Add custom message validator.
+        /// </summary>
+        /// <param name="services">service collection</param>
+        /// <param name="lifetime">specific life time for validator</param>
+        /// <param name="type">custom validator type</param>
+        /// <returns></returns>
+        public static IServiceCollection AddValidator(this IServiceCollection services, Type type,
+            ServiceLifetime lifetime = ServiceLifetime.Scoped)
+        {
+            var implementationType = type;
+            var validatorType = implementationType.GetInterfaces().FirstOrDefault(t =>
+                t.IsGenericType && t.GetGenericTypeDefinition() == typeof(IValidator<>));
+
+            if (validatorType == null)
+                throw new AggregateException(implementationType.Name + "is not implement with IValidator<>.");
+
+            var messageType = validatorType.GetGenericArguments().First();
+            var serviceType = typeof(IValidator<>).MakeGenericType(messageType);
+
+            services.Add(new ServiceDescriptor(serviceType, implementationType, lifetime));
             return services;
         }
     }
